e03b839d4c9639dc78d4c0993fef132c06f6fabf
tulolo287 <iv1055@yandex.ru>
2026-04-24 16:56:43 +0300

    sadfqegd

--- src/components/MapRegion.tsx
+++ src/components/MapRegion.tsx
@@ -3,15 +3,17 @@ import type { RegionData } from '../svgMapData';
 interface MapRegionProps {
     region: RegionData;
     onMouseEnter: (region: RegionData) => void;
+    onClick: (region: RegionData) => void;
     onMouseLeave: () => void;
 }
 
-export default function MapRegion({ region, onMouseEnter, onMouseLeave }: MapRegionProps) {
+export default function MapRegion({ region, onMouseEnter, onMouseLeave, onClick }: MapRegionProps) {
     return (
         <g key={region.id} className='region'>
             <path
                 d={region.path}
                 onMouseEnter={() => onMouseEnter(region)}
+                onClick={() => onClick(region)}
                 onMouseLeave={onMouseLeave}
             />
         </g>
